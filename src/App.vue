59ede4ea670346e951b1b97385251de2e0bd6524
Luc de Wit <luc.dewit@hotmail.nl>
2022-07-13 19:58:32 +0200

    Updated and implemented header component

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,24 @@
 <template>
   <div class="nav">
-    <RouterLink to="/">
-      <a>Home</a>
-    </RouterLink>
-    <RouterLink to="/nieuws">
-      <a>Nieuws</a>
-    </RouterLink>
-   <RouterLink to="/bibiotheek">
-      <a>Bibiotheek</a>
-   </RouterLink>
-   <RouterLink to="/over">
-      <a>Over</a>
-    </RouterLink>
+    <Header />
 
-    <!--
-    <RouterLink to="/aanmelden">
-    <a><img class="icon" src="./images/profile.png"></a>
-    </RouterLink>
-    -->
-    
-    </div>
-
-    <RouterView/>
+    <RouterView />
+  </div>
 </template>
+
 <script>
+import Header from "@/components/Header.vue"
+
 export default {
-    
+  components: {
+    Header
+  }
 }
 </script>
+
 <style>
-    .nav {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      gap: 2rem;
-      margin-top: 1rem;
-      border: 3px solid #590015;
-      border-radius: 3rem;
-    }
-
-    a {
-      text-decoration: none;
-      color: #940000;
-      font-weight: bold;
-      font-size: 1.5rem;
-      font-family: 'Gulzar', serif;
-      transition: all 0.5s ease-in-out;
-    }
-
-    .icon {
-      width: 2rem;
-      margin-top: 1.5rem;
-      height: 2rem;
-    }
-
-    a:hover {
-      color: #8d0000;
-      text-decoration: underline;
-      font-size: 1.7rem;
-    }
+body, html {
+  margin: 0;
+  padding: 0;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,49 +1,65 @@
 <template>
-    <div class="nav">
-        <RouterLink to="/">
-            <a>Home</a>
-        </RouterLink>
-        <RouterLink to="/about">
-            <a>Nieuws</a>
-        </RouterLink>
-        <RouterLink to="/bibiotheek">
-            <a>Bibiotheek</a>
-        </RouterLink>
-        <RouterLink to="/news">
-            <a>Over</a>
-        </RouterLink>
+    <div class="header">
+        <img src="@/images/pnl.jpg" alt="PNL logo" id="header-icon">
 
-        <RouterView/>
+        <div id="nav">
+            <RouterLink to="/">
+                <a>Home</a>
+            </RouterLink>
+            <RouterLink to="/about">
+                <a>Nieuws</a>
+            </RouterLink>
+            <RouterLink to="/bibiotheek">
+                <a>Bibiotheek</a>
+            </RouterLink>
+            <RouterLink to="/news">
+                <a>Over</a>
+            </RouterLink>
+        </div>
     </div>
 </template>
+
 <script>
 export default {
     
 }
 </script>
-<style>
-    .nav {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 2rem;
-        margin-top: 1rem;
-        border: 3px solid #590015;
-        border-radius: 3rem;
-    }
 
-    a {
-        text-decoration: none;
-        color: #940000;
-        font-weight: bold;
-        font-size: 1.5rem;
-        font-family: 'Gulzar', serif;
-        transition: all 0.5s ease-in-out;
-    }
+<style scoped>
+#header-icon {
+    height: 50px;
+    justify-self: left;
+    position: relative;
+    top: 5px;
+}
 
-    a:hover {
-        color: #8d0000;
-        text-decoration: underline;
-        font-size: 1.7rem;
-    }
+.header {
+    display: grid;
+    grid-template-columns: 50px auto;
+    padding: 10px;
+    
+    gap: 2rem;
+    background: #780001;
+    box-shadow: 0 5px 5px rgba(1, 1, 1, 0.5);
+}
+
+.header #nav {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+}
+
+a {
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+    font-size: 1.5rem;
+    font-family: 'Gulzar', serif;
+    transition: all 0.5s ease-in-out;
+    margin-right: 12px;
+}
+
+a:hover {
+    text-decoration: underline;
+}
 </style>
