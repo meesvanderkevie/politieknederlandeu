2df60486327b170c9d5b6e4b36afd1e29dd74a94
Luc de Wit <luc.dewit@hotmail.nl>
2022-07-13 21:40:41 +0200

    Collapsable divier component

---
+++ src/components/CollapsableDivider.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="divider">
+        <div id="left">
+            <slot name="left" />
+        </div>
+        <div id="right">
+            <slot name="right" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+#divider {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+}
+
+#right, #left {
+    padding: 10px;
+}
+</style>
